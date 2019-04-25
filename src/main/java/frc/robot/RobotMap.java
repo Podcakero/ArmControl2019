b920c4a97ac6a1d330054320cd29f8e52d783b8d
Podcakero <business.podcake@gmail.com>
2019-04-25 14:24:44 -0400

    Created talon object for the 4 drive motors and created a mecanum drive object

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,13 +7,18 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
+public class RobotMap 
+{
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
@@ -23,4 +28,65 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+  /**
+   * The CAN ID for the front left drive talon
+   */
+  public static final int FRONT_LEFT_DRIVE_TALON_ID = 1;
+  /**
+   * The CAN ID for the front right drive talon 
+   */
+  public static final int FRONT_RIGHT_DRIVE_TALON_ID = 2;
+  /**
+   * The CAN ID for the rear left drive talon
+   */
+  public static final int REAR_LEFT_DRIVE_TALON_ID = 3;
+  /**
+   * The CAN ID for the rear right drive talon
+   */
+  public static final int REAR_RIGHT_DRIVE_TALON_ID = 4;
+
+  /**
+   * The front left drive talon object
+   */
+  public static WPI_TalonSRX frontLeftDriveTalon;
+  /**
+   * The front right drive talon object
+   */
+  public static WPI_TalonSRX frontRightDriveTalon;
+  /**
+   * The rear left drive talon object
+   */
+  public static WPI_TalonSRX rearLeftDriveTalon;
+  /**
+   * The rear right drive talon object
+   */
+  public static WPI_TalonSRX rearRightDriveTalon;
+
+
+  /**
+   * This is the drivetrain object which handles the input from the controllers to move a mechanum chassis
+   */
+  public static MecanumDrive drivetrain;
+
+  /**
+   * This is where we instantiate any and all objects, as well as configure specific settings such as PID values and follow modes
+   */
+  public void init()
+  {
+    setupDrivetrain();
+  }
+
+  /**
+   * This method instantiates the drivetrain motors and sets their parameters
+   */
+  private void setupDrivetrain()
+  {
+    frontLeftDriveTalon = new WPI_TalonSRX(FRONT_LEFT_DRIVE_TALON_ID);
+    frontRightDriveTalon = new WPI_TalonSRX(FRONT_RIGHT_DRIVE_TALON_ID);
+    rearLeftDriveTalon = new WPI_TalonSRX(REAR_LEFT_DRIVE_TALON_ID);
+    rearRightDriveTalon = new WPI_TalonSRX(REAR_RIGHT_DRIVE_TALON_ID);
+
+    drivetrain = new MecanumDrive(frontLeftDriveTalon, rearLeftDriveTalon, frontRightDriveTalon, rearRightDriveTalon);
+  }
 }
